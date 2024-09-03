210705856d9fff22d3c8b2a5d5e05060f03baf78
Shuoberto <115667535+Szhenger@users.noreply.github.com>
2024-09-03 11:40:07 -0400

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,16 @@
+-- Queries the database
+
+-- Search for users
+SELECT * FROM "users";
+
+-- Search for friends
+SELECT * FROM "friends";
+
+-- Search for work
+SELECT * FROM "work";
+
+-- Search for hobbies
+SELECT * FROM "hobbies";
+
+-- Search for relations
+SELECT * FROM "friends_relations";
